f9be4d9f3755b7d13f7e8ebeebc39b76f4f083b7
nwallenfang <nils@wallenfang.de>
2022-07-11 16:36:02 +0200

    sprite3d

---
+++ Logic/Level.gd
@@ -0,0 +1,22 @@
+extends Spatial
+
+export var speed = 80.0  # meter / second
+
+const DISK = preload("res://Objects/Disk.tscn")
+
+func _ready() -> void:
+	$Tunnel.get_node("Mesh").get_active_material(0).set("shader_param/scroll_speed", speed)
+	
+	
+	var disk = DISK.instance()
+	disk.speed = speed
+	$Disks.add_child(disk)
+	disk.global_transform.origin = $DiskSpawnPosition.global_transform.origin
+
+
+func _on_SpawnDiskTimer_timeout() -> void:
+	var disk = DISK.instance()
+	disk.speed = speed
+	$Disks.add_child(disk)
+	disk.global_transform.origin = $DiskSpawnPosition.global_transform.origin
+
--- Objects/Tunnel.gd
+++ Objects/Tunnel.gd
@@ -1,9 +1,6 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
 
 
 # Called when the node enters the scene tree for the first time.
