fcfa92b8cca5b09cf6215e77353c4d222fb55d8a
nwallenfang <nils@wallenfang.de>
2022-07-11 14:58:19 +0200

    level prototyping

---
+++ Objects/Sphere.gd
@@ -0,0 +1,11 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Animation.play("Roll")
