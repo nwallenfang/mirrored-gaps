a87400ea1d61ebdff4446ecade6506481fe5495e
nheeb <nitai64@yahoo.de>
2022-07-13 16:09:17 +0200

    fuck it

--- Logic/Symmetrizer.gd
+++ Logic/Symmetrizer.gd
@@ -5,45 +5,13 @@ var currently_symmetrizing = false
 
 onready var cursor = $Cursor
 
-func _ready():
-	reset_bool_data()
-
-var bool_data := [] # list int -> {int -> bool}
-func reset_bool_data():
-	var border = 2
-	var res = Game.image_res + border
-	if bool_data.empty():
-		for i in range(res):
-			bool_data.append([])
-			for j in range(res):
-				bool_data[i].append(false)
-	else:
-		for i in range(res):
-			for j in range(res):
-				bool_data[i][j] = false
-
-func set_pixel_data(x, y):
-	#if min(x, y) >= 0 and max(x, y) < Game.image_res:
-	bool_data[x+1][y+1] = true
-
-func set_pixel_area(x, y):
-#	set_pixel_data(x+1,y+1)
-#	set_pixel_data(x+1,y)
-#	set_pixel_data(x+1,y-1)
-#	set_pixel_data(x,y+1)
-	set_pixel_data(x,y)
-#	set_pixel_data(x,y-1)
-#	set_pixel_data(x-1,y+1)
-#	set_pixel_data(x-1,y)
-#	set_pixel_data(x-1,y-1)
-
-var number_of_frames = 20  # to spread the workload over
+var number_of_frames = 30  # to spread the workload over
 func calc_symm(im: Image, cursor_pos: Vector2, rotation: float): #  -> Image:
 	var res = Game.image_res
 
 	var result : Image = Image.new()
+	
 	result.create(res, res, false, Image.FORMAT_RGBA8)
-	reset_bool_data()
 
 	yield(get_tree(), "idle_frame")
 
@@ -54,33 +22,39 @@ func calc_symm(im: Image, cursor_pos: Vector2, rotation: float): #  -> Image:
 	result.fill(Color.white)
 	yield(get_tree(), "idle_frame")
 	im.lock()
+	result.lock()
+
 	for i in range(res):
 		if rows_this_frame >= rows_per_frame:
 			rows_this_frame = 0
 			yield(get_tree(), "idle_frame")
 		for j in range(res):
 			if im.get_pixel(i, j) == Color.black:
-				set_pixel_area(i, j)
+				result.set_pixel(i, j, Color.black)
+				if i < Game.image_res-1 and i > 0 and j < Game.image_res-1 and j > 0:
+					result.set_pixel(i+1, j+1, Color.black)
+					result.set_pixel(i+1, j, Color.black)
+					result.set_pixel(i+1, j-1, Color.black)
+					result.set_pixel(i, j+1, Color.black)
+					result.set_pixel(i, j-1, Color.black)
+					result.set_pixel(i-1, j+1, Color.black)
+					result.set_pixel(i-1, j, Color.black)
+					result.set_pixel(i-1, j-1, Color.black)
 				var pos = Vector2(i, j)
 				var mirrored = (pos - cursor_pos).reflect(rotation_vec) + cursor_pos
 				if mirrored.x < Game.image_res-1 and mirrored.x > 0 and mirrored.y < Game.image_res-1 and mirrored.y > 0:
-					set_pixel_area(mirrored.x, mirrored.y)
+					result.set_pixel(int(mirrored.x), int(mirrored.y), Color.black)
+					result.set_pixel(int(mirrored.x+1), int(mirrored.y), Color.black)
+					result.set_pixel(int(mirrored.x-1), int(mirrored.y), Color.black)
+					result.set_pixel(int(mirrored.x), int(mirrored.y+1), Color.black)
+					result.set_pixel(int(mirrored.x), int(mirrored.y-1), Color.black)
+					result.set_pixel(int(mirrored.x-1), int(mirrored.y-1), Color.black)
+					result.set_pixel(int(mirrored.x+1), int(mirrored.y-1), Color.black)
+					result.set_pixel(int(mirrored.x-1), int(mirrored.y+1), Color.black)
+					result.set_pixel(int(mirrored.x+1), int(mirrored.y+1), Color.black)
 		rows_this_frame += 1
-	im.unlock()
-
-	result.lock()
-	for x in range(Game.image_res):
-		rows_this_frame += 1
-		if rows_this_frame >= rows_per_frame:
-			rows_this_frame = 0
-			yield(get_tree(), "idle_frame")
-		for y in range(Game.image_res):
-			var draw = bool_data[x+2][y+1] or bool_data[x][y+1] or bool_data[x+1][y+2] or bool_data[x+1][y]
-			if !draw:
-				draw = draw or bool_data[x+2][y+2] or bool_data[x][y] or bool_data[x+2][y] or bool_data[x][y+2]  
-			if draw:
-				result.set_pixel(x, y, Color.black)
 	result.unlock()
+	im.unlock()
 
 	return result
 
