0127b50fe8d4176d08b3c2d0e0b6f00a60b1cf12
nheeb <nitai64@yahoo.de>
2022-07-13 15:58:54 +0200

    Mehr Performance

--- Logic/Symmetrizer.gd
+++ Logic/Symmetrizer.gd
@@ -5,30 +5,45 @@ var currently_symmetrizing = false
 
 onready var cursor = $Cursor
 
-var bool_data := {} # map vec2 -> bool
+func _ready():
+	reset_bool_data()
+
+var bool_data := [] # list int -> {int -> bool}
+func reset_bool_data():
+	var border = 2
+	var res = Game.image_res + border
+	if bool_data.empty():
+		for i in range(res):
+			bool_data.append([])
+			for j in range(res):
+				bool_data[i].append(false)
+	else:
+		for i in range(res):
+			for j in range(res):
+				bool_data[i][j] = false
 
 func set_pixel_data(x, y):
 	#if min(x, y) >= 0 and max(x, y) < Game.image_res:
-	bool_data[Vector2(x,y)] = true
+	bool_data[x+1][y+1] = true
 
 func set_pixel_area(x, y):
-	set_pixel_data(x+1,y+1)
-	set_pixel_data(x+1,y)
-	set_pixel_data(x+1,y-1)
-	set_pixel_data(x,y+1)
+#	set_pixel_data(x+1,y+1)
+#	set_pixel_data(x+1,y)
+#	set_pixel_data(x+1,y-1)
+#	set_pixel_data(x,y+1)
 	set_pixel_data(x,y)
-	set_pixel_data(x,y-1)
-	set_pixel_data(x-1,y+1)
-	set_pixel_data(x-1,y)
-	set_pixel_data(x-1,y-1)
+#	set_pixel_data(x,y-1)
+#	set_pixel_data(x-1,y+1)
+#	set_pixel_data(x-1,y)
+#	set_pixel_data(x-1,y-1)
 
-var number_of_frames = 30  # to spread the workload over
+var number_of_frames = 20  # to spread the workload over
 func calc_symm(im: Image, cursor_pos: Vector2, rotation: float): #  -> Image:
 	var res = Game.image_res
 
 	var result : Image = Image.new()
 	result.create(res, res, false, Image.FORMAT_RGBA8)
-	bool_data = {}
+	reset_bool_data()
 
 	yield(get_tree(), "idle_frame")
 
@@ -52,21 +67,19 @@ func calc_symm(im: Image, cursor_pos: Vector2, rotation: float): #  -> Image:
 					set_pixel_area(mirrored.x, mirrored.y)
 		rows_this_frame += 1
 	im.unlock()
-	yield(get_tree(), "idle_frame")
-	var counter = 0
+
 	result.lock()
-	print(len(bool_data))
-	for xy in bool_data.keys():
-		var x = int(xy.x)
-		var y = int(xy.y)
-		if min(x, y) < 0 or max(x, y) >= Game.image_res:
-			continue
-		counter += 1
-		if counter >= 20000:
-			counter = 0
+	for x in range(Game.image_res):
+		rows_this_frame += 1
+		if rows_this_frame >= rows_per_frame:
+			rows_this_frame = 0
 			yield(get_tree(), "idle_frame")
-		if bool_data[xy]:
-			result.set_pixel(xy[0], xy[1], Color.black)
+		for y in range(Game.image_res):
+			var draw = bool_data[x+2][y+1] or bool_data[x][y+1] or bool_data[x+1][y+2] or bool_data[x+1][y]
+			if !draw:
+				draw = draw or bool_data[x+2][y+2] or bool_data[x][y] or bool_data[x+2][y] or bool_data[x][y+2]  
+			if draw:
+				result.set_pixel(x, y, Color.black)
 	result.unlock()
 
 	return result
